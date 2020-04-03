d5da099fa64ac50bcad81a18c01823fd7737492a
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-03 19:11:15 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,27 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres, through: :songs
+  has_many :genres, 
+  through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
-  end
+    self.genres.first
+  end 
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.count
   end
+  
+  def find_or_create_by(name:)
+    if !artist
+      Artist.new(name: name)
+    else
+      artist = self.create(name: name)
+    end 
+    artist
+  end
+
 end
