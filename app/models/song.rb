845ef8834fb01b7152f7d7b60ce7b38acead420f
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-05 14:50:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,11 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+<<<<<<< HEAD
      drake = Artist.find_or_create_by(name: "Drake")
      self.artist = drake
+=======
+     Artist.find_or_create_by(name: "Drake")
+>>>>>>> 541205519ca53f023af8f1fdc38cf2f4e11bcb38
    end
 end
